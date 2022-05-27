5b170480f2fa73a5a54e15b7be6baefb11a06fed
MayyarJavaProgrammer <mayarzidane1@gmail.com>
2022-05-27 19:53:59 +0300

    add hello in b1

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -35,10 +35,7 @@ public class GitTest {
         System.out.println("your age is :" + userage);
         System.out.println("your favorite meal is :" + meal);
         System.out.println("your favorite sport is :" + sport );
-        System.out.println("-------------finish for b1---------------");
-        System.out.println("this is for b1");
-        System.out.println("this is for b1");
-    
+        System.out.println("hello in b1");
     }
 
 }
